341b08fe5c1f880853a8526b3cb0435af01a9e27
Balint Erdi <balint.erdi@gmail.com>
2009-06-03 09:24:47 +0200

    adding 'one line from file' module
    Signed-off-by: Balint Erdi <balint.erdi@gmail.com>

---
+++ modules/one_line_from_file.rb
@@ -0,0 +1,14 @@
+# require 'scrubyt'
+require "rubygems"
+require 'open-uri'
+require "hpricot"
+
+module OneLineFromFile
+  # read all lines from the file that contains the strategies
+  # select the strategies that are not used yet
+  # pick a random strategy
+  
+  # store used strategies in a file with (store the md5 sum of the strategies' text)
+end
+
+# Daily_Oblique/ZenKoan77
