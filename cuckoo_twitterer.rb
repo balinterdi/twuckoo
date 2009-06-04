3191eec8510e94d4cbad984dfd5e5b81e3a49382
Balint Erdi <balint.erdi@gmail.com>
2009-06-04 10:05:49 +0200

    picks a next line to be tweeted
    Signed-off-by: Balint Erdi <balint.erdi@gmail.com>

--- cuckoo_twitterer.rb
+++ cuckoo_twitterer.rb
@@ -1,5 +1,26 @@
 class CuckooTwitterer
+
+  #TODO: move most of these functions to the module (maybe except the tweet method)
+  attr_reader :fodder
+
+  def initialize
+    @fodder = []
+  end
+
+  def add_fodder(*fodder)
+    @fodder.concat(fodder)
+  end
+
+  def next
+    fodder.delete_at(pick)
+  end
+
   def tweet
+    self.next
+  end
+
+  def pick
+    rand(fodder.length)
   end
 end
 
--- spec/one_line_from_file_spec.rb
+++ spec/one_line_from_file_spec.rb
@@ -1,13 +1,50 @@
 require "spec"
+require "mocha"
+
 require File.join(File.dirname(__FILE__), '..', 'cuckoo_twitterer')
 require File.join(File.dirname(__FILE__), '..', 'modules', 'one_line_from_file')
+
+# use mocha for mocking instead of
+# Rspec's own mock framework
+Spec::Runner.configure do |config|
+  config.mock_with :mocha
+end
 
 class CuckooTwittererWithOneLineFromFileSpec
   describe "A cuckoo twitterer with one line from a file" do
     before do
       CuckooTwitterer.send(:include, OneLineFromFile)
       @cuckoo = CuckooTwitterer.new
+      # @cuckoo.add_fodder("I am happy", "I am sad", "I am thrilled", "I am bored")
     end
-    
+
+    it "stores a line that was sent to it correctly" do
+      @cuckoo.fodder.should be_empty
+      @cuckoo.add_fodder("I am happy", "I am sad")
+      @cuckoo.fodder.should == ["I am happy", "I am sad"]
+    end
+
+    describe "when it tweeted a line already" do
+      before do
+        @cuckoo.stubs(:fodder).returns(["I am happy", "I am sad"])
+        @cuckoo.stubs(:pick).returns(0)
+        @cuckoo.tweet
+      end
+
+      it "it removes it" do
+        @cuckoo.fodder.should == ["I am sad"]
+      end
+
+      it "it picks an unused line to tweet next" do
+        @cuckoo.next.should == "I am sad"
+      end
+
+    end
+
+    it "returns the used lines correctly" do
+      @cuckoo.stubs(:use).returns()
+      @cuckoo
+    end
+
   end
 end
